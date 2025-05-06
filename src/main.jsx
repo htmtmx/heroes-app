b9d1c56001a3cc8168a36fe7d3ac0ddd516e849a
Cesar Pineda <cesar.pineda@kodevox.com>
2025-05-06 17:28:13 -0600

    ♻️ Update import statement for BrowserRouter and format JSX in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router';
 import { HeroesApp } from './HeroesApp.jsx'
 
 import './styles.css'
@@ -8,7 +8,7 @@ import './styles.css'
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <HeroesApp/>
+            <HeroesApp />
         </BrowserRouter>
-  </StrictMode>,
+    </StrictMode>,
 )
