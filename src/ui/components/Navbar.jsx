3709214f0d45ed6ae842ed91997904f99c7ee7d3
Cesar Pineda <cesar.pineda@kodevox.com>
2025-05-06 17:27:45 -0600

    💄 Refactor Navbar component to use NavLink and improve layout

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,43 +1,35 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from "react-router";
 
 export const Navbar = () => {
+    
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-
-            <Link
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+            <NavLink
                 className="navbar-brand"
-                to="/"
-            >
+                to="/" end>
                 Asociaciones
-            </Link>
-
+            </NavLink>
             <div className="navbar-collapse">
                 <div className="navbar-nav">
-
                     <NavLink
-                        className="nav-item nav-link"
-                        to="/marvel"
-                    >
+                        className={`nav-item nav-link ${({ isActive }) => isActive ? "active" : ""}`}
+                        to="/marvel" end>
                         Marvel
                     </NavLink>
 
                     <NavLink
-                        className="nav-item nav-link"
-                        to="/dc"
-                    >
+                        className={`nav-item nav-link ${({ isActive }) => isActive ? "active" : ""}`}
+                        to="/dc">
                         DC
                     </NavLink>
                 </div>
             </div>
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink
-                        className="nav-item nav-link"
-                        to="auth"
-                    >
-                        Logout
-                    </NavLink>
+                    <span className='nav-item nav-link text-primary'>
+                        Cesar
+                    </span>
+                    <button className='nav-item nav-link btn'>Logout</button>
                 </ul>
             </div>
         </nav>
