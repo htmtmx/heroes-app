3ef90763223465b397bc76ad2fb1366134ac63b8
Cesar Pineda <cesar.pineda@kodevox.com>
2025-05-15 11:40:31 -0600

    💄 Update Dc and Marvel components to display publisher names and include HeroesList

--- src/heroes/pages/Dc.jsx
+++ src/heroes/pages/Dc.jsx
@@ -1,5 +1,12 @@
+import { HeroesList } from '../components'
+
 export const Dc = () => {
-  return (
-    <div>Dc</div>
-  )
+    return (
+        <>
+            <div>DC Comics</div>
+            <hr />
+            <HeroesList publisher='DC Comics' />
+        </>
+
+    )
 }
--- src/heroes/pages/Marvel.jsx
+++ src/heroes/pages/Marvel.jsx
@@ -1,6 +1,12 @@
+import { HeroesList } from '../components'
+
 export const Marvel = () => {
-  return (
-    <div>Marvel</div>
-  )
+    return (
+        <>
+            <div>Marvel Comics</div>
+            <hr />
+            <HeroesList publisher='Marvel Comics'/>
+        </>
+    )
 }
 
