3c799d38575d846dbe670181eec24e6eadf361f3
Cesar Pineda <cesar.pineda@kodevox.com>
2025-05-15 11:43:29 -0600

    ✨ Add heroes data and image URLs for DC and Marvel characters

---
+++ src/heroes/data/urlImgs.js
@@ -0,0 +1,43 @@
+import arrow from  '../../assets/dc-arrow.jpg';
+import batman from  '../../assets/dc-batman.jpg';
+import black from  '../../assets/dc-black.jpg';
+import blue from  '../../assets/dc-blue.jpg';
+import flash from  '../../assets/dc-flash.jpg';
+import green from  '../../assets/dc-green.jpg';
+import martian from  '../../assets/dc-martian.jpg';
+import robin from  '../../assets/dc-robin.jpg';
+import superman from  '../../assets/dc-superman.jpg';
+import wonder from  '../../assets/dc-wonder.jpg';
+import marvspider from  '../../assets/marvel-spider.jpg';
+import marvcaptain from  '../../assets/marvel-captain.jpg';
+import marviron from  '../../assets/marvel-iron.jpg';
+import marvthor from  '../../assets/marvel-thor.jpg';
+import marvhulk from  '../../assets/marvel-hulk.jpg';
+import marvwolverine from  '../../assets/marvel-wolverine.jpg';
+import marvdaredevil from  '../../assets/marvel-daredevil.jpg';
+import marvhawkeye from  '../../assets/marvel-hawkeye.jpg';
+import marvcyclops from  '../../assets/marvel-cyclops.jpg';
+import marvsilver from  '../../assets/marvel-silver.jpg';
+
+export const urlImgs = {
+    'dc-arrow': arrow,
+    'dc-batman': batman,
+    'dc-black': black,
+    'dc-blue': blue,
+    'dc-flash': flash,
+    'dc-green': green,
+    'dc-martian': martian,
+    'dc-robin': robin,
+    'dc-superman': superman,
+    'dc-wonder': wonder,
+    'marvel-spider': marvspider,
+    'marvel-captain': marvcaptain,
+    'marvel-iron': marviron,
+    'marvel-thor': marvthor,
+    'marvel-hulk': marvhulk,
+    'marvel-wolverine': marvwolverine,
+    'marvel-daredevil': marvdaredevil,
+    'marvel-hawkeye': marvhawkeye,
+    'marvel-cyclops': marvcyclops,
+    'marvel-silver': marvsilver,
+}
